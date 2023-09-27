daaf0eea015e3770dfc04c84f2c0e228f07c38f1
DanikGuun <80112309+DanikGuun@users.noreply.github.com>
2023-09-28 02:20:14 +0300

    С горем пополам запилил добавление кнопок

--- src/main/kotlin/Bot/Bot.kt
+++ src/main/kotlin/Bot/Bot.kt
@@ -3,21 +3,66 @@ package Bot
 import Controler.Controller
 import org.telegram.telegrambots.bots.TelegramLongPollingBot
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage
-import org.telegram.telegrambots.meta.api.objects.Message
 import org.telegram.telegrambots.meta.api.objects.Update
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.InlineKeyboardMarkup
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.InlineKeyboardButton
 
 class Bot(val controller: Controller): TelegramLongPollingBot(){
     override fun onUpdateReceived(update: Update) {
-        val chatID = update.message.chatId
-        sendMessage("aspdgkfh", chatID)
+        if(update.callbackQuery != null){
+            sendMessage(update.callbackQuery.from.id.toString(), update.callbackQuery.data)
+        }
+        else {
+            val list = createButtonsRowList(
+                arrayListOf(
+                    arrayListOf(
+                        arrayListOf("Стёпа лох", "Степа не лох"),
+                        arrayListOf("Данила лох", "бля")
+                    )
+                )
+            )
+            sendMessage(update.message.chatId.toString(), "степа гей", list)
+        }
     }
 
-    fun sendMessage(text: String, chatID: Long){
+    fun sendMessage(chatID: String, text: String, buttons: ArrayList<List<InlineKeyboardButton>>? = null){
         val message = SendMessage()
+        message.enableMarkdown(true)
         message.setChatId(chatID)
         message.text = text
+        if(buttons != null){
+            val inlineKeyboardMarkup = InlineKeyboardMarkup()
+            inlineKeyboardMarkup.keyboard = buttons
+            message.replyMarkup = inlineKeyboardMarkup
+        }
         execute(message)
     }
+
+    //список со строчками 0 - все строчки, 1 - все кнопки, 2 - текст и колбек кнопки
+    fun createButtonsRowList(buttonsData: ArrayList<ArrayList<ArrayList<String>>>): ArrayList<List<InlineKeyboardButton>>{
+        val buttonsArray = ArrayList<List<InlineKeyboardButton>>()
+        for(row in buttonsData){
+            buttonsArray.add(createRowOfButtons(row))
+        }
+        return buttonsArray
+    }
+
+    //делает строчку
+    fun createRowOfButtons(buttonsData: ArrayList<ArrayList<String>>): List<InlineKeyboardButton>{
+        val row = ArrayList<InlineKeyboardButton>()
+        for(button in buttonsData){
+            row.add(createInlineButton(button[0], button[1]))
+        }
+        return row.toList()
+    }
+    //кнопку делает епт
+    fun createInlineButton(text: String, callbackData: String): InlineKeyboardButton{
+        val button = InlineKeyboardButton()
+        button.text = text
+        button.callbackData = callbackData
+        return button
+    }
+
 
     override fun getBotUsername() = "svegshop_bot"
     override fun getBotToken() = "5929869980:AAE4GvBGulph377qbdpKc6xF75M069g-9ec"
--- src/main/kotlin/Controler/Controller.kt
+++ src/main/kotlin/Controler/Controller.kt
@@ -4,5 +4,8 @@ import Json.JsonHandler
 import YMoney.YMoneyHandler
 
 class Controller(val moneyHandler: YMoneyHandler, val jsonHandler: JsonHandler){
-
+    val adminID = arrayOf("")
+    fun processMessage(text: String, chatID: Long): String{
+        return ""
+    }
 }
